23cd928ac58c948fec3c15556bfe028297fd98aa
Jacob Lee <jacobhylee@gmail.com>
2022-07-01 06:56:38 +1000

    Co-authored-by: Ju An Kang <doubtful.mr@gmail.com>

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // this should alert when opening index.html
 
 const tiles = document.querySelectorAll(".tile");
+console.log("tiles", tiles);
 const playerx = "X";
 const playero = "O";
 let turn = playerx;
@@ -16,8 +17,26 @@ const strike = document.getElementById("strike");
 const gameOverArea = document.getElementById("game-over-area");
 const gameOverText = document.getElementById("game-over-text");
 const playAgain = document.getElementById("play-again");
+playAgain.addEventListener("click", startNewGame);
 
 tiles.forEach((tile) => tile.addEventListener("click", tileClick));
+
+function setHoverText() {
+  tiles.forEach((tile) => {
+    tile.classList.remove("x-hover");
+    tile.classList.remove("o-hover");
+  });
+
+  const hoverClass = `${turn.toLowerCase()}-hover`;
+
+  tiles.forEach((tile) => {
+    if (tile.innerText == "") {
+      tile.classList.add(hoverClass);
+    }
+  });
+}
+
+setHoverText();
 
 function tileClick(event) {
   if (gameOverArea.classList.contains("visible")) {
@@ -39,4 +58,74 @@ function tileClick(event) {
     boardState[tileNumber - 1] = playero;
     turn = playerx;
   }
+
+  setHoverText();
+  checkWinner();
+
+  console.log("board state is", boardState);
+}
+
+// so I'm guessing here you're tyring to do something like
+function checkWinner() {
+  for (const winningCombination of winningCombinations) {
+    const { combo, strikeClass } = winningCombination;
+    const tileValue1 = boardState[combo[0] - 1];
+    const tileValue2 = boardState[combo[1] - 1];
+    const tileValue3 = boardState[combo[2] - 1];
+
+    if (
+      tileValue1 != null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3
+    ) {
+      strike.classList.add(strikeClass);
+      gameOverScreen(tileValue1);
+      return;
+    }
+    // my code
+    // Something similar to this where you're checking if all of
+    // them are of the same shape right
+    // winningCombination.combo.every((con) => con === winningCombination.combo[0]);
+  }
+
+  const allTileFilledIn = boardState.every((tile) => tile !== null);
+  if (allTileFilledIn) {
+    gameOverScreen(null);
+  }
+}
+
+function gameOverScreen(winnerText) {
+  let text = "Draw!";
+  if (winnerText != null) {
+    text = `Winner is ${winnerText}!`;
+  }
+  gameOverArea.className = "visible";
+  gameOverText.innerText = text;
+}
+
+function startNewGame() {
+  strike.className = "strike";
+  gameOverArea.className = "hidden";
+  boardState.fill(null);
+  tiles.forEach((tile) => (tile.innerText = ""));
+  turn = playerx;
+  setHoverText();
+}
+
+const winningCombinations = [
+  { combo: [1, 2, 3], strikeClass: "strike-row-1" },
+  { combo: [4, 5, 6], strikeClass: "strike-row-2" },
+  { combo: [7, 8, 9], strikeClass: "strike-row-3" },
+  { combo: [1, 4, 7], strikeClass: "strike-column-1" },
+  { combo: [2, 5, 8], strikeClass: "strike-column-2" },
+  { combo: [3, 6, 9], strikeClass: "strike-column-3" },
+  { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
+  { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
+];
+
+//I wanted to do something like this at first
+//But I don't know how to call the grids in the if parameter properly
+
+if (boardState[0] === playerx) {
+  console.log("x is in grid 1");
 }
